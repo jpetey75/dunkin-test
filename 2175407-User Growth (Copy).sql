4165e5fee73002fa60ac9ccafa05a7485982b53e
Jake Peterson <jake@popsql.com>
2023-07-04 15:18:29 +0000

    Update query `User Growth (Copy)`

--- 2175407-User Growth (Copy).sql
+++ 2175407-User Growth (Copy).sql
@@ -1,5 +1,7 @@
 select 
-    date(users.created_at) as date, count(id) as users 
+    date(users.created_at) as date, 
+    count(id) as users 
+
 from ecommerce.users
 group by 1 
 order by 1 desc 
