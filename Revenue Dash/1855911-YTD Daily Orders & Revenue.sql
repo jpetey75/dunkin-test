1b4e7c21a0b5b09c4297d08d5d66e2c685f41086
Jake Peterson <jake@popsql.com>
2023-08-23 23:35:14 +0000

    Update query `YTD Daily Orders & Revenue`

--- Revenue Dash/1855911-YTD Daily Orders & Revenue.sql
+++ Revenue Dash/1855911-YTD Daily Orders & Revenue.sql
@@ -1,8 +1,8 @@
 select 
     date(o.created_at) as created_date,
     cast(sum(oi.sale_price) as integer) as total,
-    count(distinct o.order_id) as orders
     sum(o.num_of_item) as items,
+    count(distinct o.order_id) as orders
 
 from ecommerce.order_items oi
 
