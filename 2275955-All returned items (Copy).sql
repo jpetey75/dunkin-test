20081a414380ffa7e5070eecc865187ef939ab3c
Jake Peterson <jake@popsql.com>
2023-09-07 16:03:23 +0000

    Update query `All returned items (Copy)`

--- 2275955-All returned items (Copy).sql
+++ 2275955-All returned items (Copy).sql
@@ -16,7 +16,7 @@ with returns as (
 select 
     date(returns.returned_at) as returned_date 
     sum(returns.returned_amount) as returned_amount,
-    count(distinct order_item_id) as 
+    count(distinct order_item_id) as returned_items 
 
 from returns 
 group by 1
