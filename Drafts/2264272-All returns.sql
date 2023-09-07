9f65b5f82f5f118c2c063a6f9e1bc379d705e70d
Jake Peterson <jake@popsql.com>
2023-09-07 15:51:11 +0000

    Update query `All returns`

--- Drafts/2264272-All returns.sql
+++ Drafts/2264272-All returns.sql
@@ -1,13 +1,4 @@
 select 
-    date(oi.returned_at) as returned_date,
-    sum(sale_price) as return_amount, 
-    count(id) as items_returned
-from ecommerce.order_items oi 
-group by 1
-order by 1;
-
-
-select 
     date(oi.returned_at) as returned_date, 
     sum(cast(round(oi.sale_price, 2) as int)) as returned_amount,
     count(oi.id) as items_returned
