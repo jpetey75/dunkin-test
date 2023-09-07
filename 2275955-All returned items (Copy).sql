46af8f8c267d5fc5f2cbf6d9f9d9b864520d07fa
Jake Peterson <jake@popsql.com>
2023-09-07 16:02:18 +0000

    Create query `All returned items (Copy)`

---
+++ 2275955-All returned items (Copy).sql
@@ -0,0 +1,12 @@
+select 
+    oi.id as order_item_id,
+    oi.product_id,
+    oi.order_id,
+    oi.user_id,
+    oi.sale_price,
+    oi.returned_at,
+    cast(round(oi.sale_price, 2) as numeric) as returned_amount
+
+from ecommerce.order_items oi 
+where oi.returned_at is not null
+    and oi.status = 'Returned'
