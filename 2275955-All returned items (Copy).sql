5f5a71ee9394638ef99300ed358db6f59baab23b
Jake Peterson <jake@popsql.com>
2023-09-07 16:02:32 +0000

    Update query `All returned items (Copy)`

--- 2275955-All returned items (Copy).sql
+++ 2275955-All returned items (Copy).sql
@@ -1,13 +1,13 @@
 with returns as (
     select 
-    oi.id as order_item_id,
-    oi.product_id,
-    oi.order_id,
-    oi.user_id,
-    oi.sale_price,
-    oi.returned_at,
-    cast(round(oi.sale_price, 2) as numeric) as returned_amount
+        oi.id as order_item_id,
+        oi.product_id,
+        oi.order_id,
+        oi.user_id,
+        oi.sale_price,
+        oi.returned_at,
+        cast(round(oi.sale_price, 2) as numeric) as returned_amount
 
-from ecommerce.order_items oi 
-where oi.returned_at is not null
-    and oi.status = 'Returned'
+    from ecommerce.order_items oi 
+    where oi.returned_at is not null
+        and oi.status = 'Returned'
