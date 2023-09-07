5d180a6fe97ef2e52064b1ef99be089956ff677d
Jake Peterson <jake@popsql.com>
2023-09-07 15:54:29 +0000

    Update query `All returns`

--- Drafts/2264272-All returns.sql
+++ Drafts/2264272-All returns.sql
@@ -1,7 +1,7 @@
 select 
     oi.id as order_item_id,
     oi.returned_at,
-    to+(round(oi.sale_price, 2) as double) as returned_amount
+    to_double(round(oi.sale_price, 2) as double) as returned_amount
 
 from ecommerce.order_items oi 
 where oi.returned_at is not null
