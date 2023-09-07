4b896a20aba477a75b66fa54f6c6b8c811ea4e51
Jake Peterson <jake@popsql.com>
2023-09-07 16:12:38 +0000

    Update query `All returned items (Copy)`

--- 2275955-All returned items (Copy).sql
+++ 2275955-All returned items (Copy).sql
@@ -1,4 +1,5 @@
 with returns as (
+    
     select 
         oi.id as order_item_id,
         oi.product_id,
@@ -14,7 +15,7 @@ with returns as (
         and oi.returned_at < '2022-01-01'        
         and oi.returned_at >= '2021-01-01'    
         and oi.status = 'Returned'
-        
+
 )
 
 select 
