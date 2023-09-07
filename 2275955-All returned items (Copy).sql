bae59fa06250ba5c1f380bee366762626cf10646
Jake Peterson <jake@popsql.com>
2023-09-07 16:12:32 +0000

    Update query `All returned items (Copy)`

--- 2275955-All returned items (Copy).sql
+++ 2275955-All returned items (Copy).sql
@@ -9,6 +9,7 @@ with returns as (
         cast(round(oi.sale_price, 2) as numeric) as returned_amount
 
     from ecommerce.order_items oi 
+    
     where oi.returned_at is not null
         and oi.returned_at < '2022-01-01'        
         and oi.returned_at >= '2021-01-01'    
