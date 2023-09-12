95bb5bf951e7722cb9d321df8bfa2f8a0741e7e7
Jake Peterson <jake@popsql.com>
2023-09-12 15:43:36 +0000

    Move query `Returned Order Items and Amounts in 2021` (part 2/2)

---
+++ 2264272-Returned Order Items and Amounts in 2021.sql
@@ -0,0 +1,14 @@
+select 
+    oi.id as order_item_id,
+    oi.product_id,
+    oi.order_id,
+    oi.user_id,
+    oi.sale_price,
+    oi.returned_at,
+    cast(round(oi.sale_price, 2) as numeric) as returned_amount
+
+from ecommerce.order_items oi 
+where oi.returned_at is not null
+    and oi.status = 'Returned'
+    and oi.returned_at >= '2021-01-01'    
+    and oi.returned_at < '2022-01-01'
