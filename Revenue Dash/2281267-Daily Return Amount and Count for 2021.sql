df5bedd34e6a5381dc5a56f0bd6af45116a8b99a
Jake Peterson <jake@popsql.com>
2023-09-12 15:44:01 +0000

    Move query `Daily Return Amount and Count for 2021` (part 2/2)

---
+++ Revenue Dash/2281267-Daily Return Amount and Count for 2021.sql
@@ -0,0 +1,24 @@
+with returned_items as (
+    select 
+        oi.id as order_item_id,
+        oi.product_id,
+        oi.order_id,
+        oi.user_id,
+        oi.sale_price,
+        oi.returned_at,
+        cast(round(oi.sale_price, 2) as numeric) as returned_amount
+
+    from ecommerce.order_items oi 
+    where oi.returned_at is not null
+        and oi.status = 'Returned'
+        and oi.returned_at >= '2021-01-01'    
+        and oi.returned_at < '2022-01-01'
+)
+
+select 
+    date(returned_at) as return_date, 
+    sum(returned_amount) as return_amount, 
+    count(*) as returned_items 
+    
+from returned_items
+group by 1
