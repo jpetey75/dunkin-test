6c2069eec72dc782fbd189bbe1b4bd06975477a5
Nina Doeff <nina@popsql.com>
2023-09-15 22:50:17 +0000

    Create query `Items in Order`

---
+++ Drafts/2107947-Items in Order.sql
@@ -0,0 +1,21 @@
+select 
+    num_of_item, 
+    round(count(1)/100)*100 as ct
+
+from ecommerce.orders o
+
+where date(o.created_at) >= '2022-03-01'
+    and status in ({{status}})
+
+group by 1 
+order by 1select 
+    num_of_item, 
+    round(count(1)/100)*100 as ct
+
+from ecommerce.orders o
+
+where date(o.created_at) >= '2022-03-01'
+    and status in ({{status}})
+
+group by 1 
+order by 1
