8da0ec3a8beb5a7652714772601a42f7eacceb7f
Jake Peterson <jake@popsql.com>
2023-10-02 16:23:02 +0000

    Update query `Monthly Department Big numbers`

--- dbt Orders Dashboard/2311151-Monthly Department Big numbers.sql
+++ dbt Orders Dashboard/2311151-Monthly Department Big numbers.sql
@@ -3,7 +3,7 @@ select
     round(sum(order_total_mens)) as mens,
     round(sum(order_total_womens)) as womens
 from dbt.orders 
-where 
+where order_status in ({{order_status}})
 group by 1 
 order by 1 desc  
 limit 2
