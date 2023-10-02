aef1ee81d830b77840b951736a07540ff4f8e31f
Jake Peterson <jake@popsql.com>
2023-10-02 16:06:27 +0000

    Update query `Monthly Revenue by Department`

--- dbt Orders Dashboard/2311134-Monthly Revenue by Department.sql
+++ dbt Orders Dashboard/2311134-Monthly Revenue by Department.sql
@@ -1,7 +1,7 @@
 select 
     order_created_at,
-    round(sum(order_total_mens)) as mens
-    round(sum(order_total_mens)) as revenue
+    round(sum(order_total_mens)) as mens,
+    round(sum(order_total_mens)) as womens
 from dbt.orders 
 group by 1 
 order by 1
