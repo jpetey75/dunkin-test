a45209d17c4617246c0d38a3eec6fdfc8ce48bc7
Jake Peterson <jake@popsql.com>
2023-10-02 16:15:38 +0000

    Update query `Monthly Department Big numbers`

--- dbt Orders Dashboard/2311151-Monthly Department Big numbers.sql
+++ dbt Orders Dashboard/2311151-Monthly Department Big numbers.sql
@@ -1,6 +1,6 @@
 select 
     order_created_month,
-    round(sum(order_total_mens)) as mens,
+    round(sum(order_total_mens),) as mens,
     round(sum(order_total_womens)) as womens
 from dbt.orders 
 group by 1 
