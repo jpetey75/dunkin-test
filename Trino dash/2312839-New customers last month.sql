11219d82d47e196dd9015fe24bea009e1d50e427
Jake Peterson <jake@popsql.com>
2023-10-03 15:07:18 +0000

    Update query `New customers last month`

--- Trino dash/2312839-New customers last month.sql
+++ Trino dash/2312839-New customers last month.sql
@@ -19,7 +19,7 @@ total as (
 
     select 
         dd.d_date,
-        count(ss.ss_customer_sk) as customers ,
+        count(distinct ss.ss_customer_sk) as customers,
         sum(ss.ss_net_paid) as total_paid  
 
     from sf1.store_sales ss 
