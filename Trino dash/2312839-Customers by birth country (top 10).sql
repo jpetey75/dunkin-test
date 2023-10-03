dac6dfb6b49604378a39134d45143d4a32bdf053
Jake Peterson <jake@popsql.com>
2023-10-03 15:05:08 +0000

    Update query `Customers by birth country (top 10)`

--- Trino dash/2312839-Customers by birth country (top 10).sql
+++ Trino dash/2312839-Customers by birth country (top 10).sql
@@ -6,6 +6,8 @@ from sf1.customer c
     
     inner join sf1.date_dim dd 
         on dd.d_date_sk = c.c_first_sales_date_sk 
+        and dd.d_date >= date '1998-02-01'
+        and dd.d_date < date '1998-03-01'
 
 where c_birth_country is not null 
 
