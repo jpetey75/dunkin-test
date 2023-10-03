5ef41a1e1d83bebd6a360e5d7130fded1b5bb8ef
Jake Peterson <jake@popsql.com>
2023-10-03 15:05:51 +0000

    Update query `New customers last month`

--- Trino dash/2312839-New customers last month.sql
+++ Trino dash/2312839-New customers last month.sql
@@ -1,16 +1,16 @@
 with new as (
     select 
-    dd.d_date,
-    count(1) as customers 
+        dd.d_date,
+        count(1) as customers 
 
-from sf1.customer c 
-    
-    inner join sf1.date_dim dd 
-        on dd.d_date_sk = c.c_first_sales_date_sk 
-        and dd.d_date >= date '1998-02-01'
-        and dd.d_date < date '1998-03-01'
+    from sf1.customer c 
+        
+        inner join sf1.date_dim dd 
+            on dd.d_date_sk = c.c_first_sales_date_sk 
+            and dd.d_date >= date '1998-02-01'
+            and dd.d_date < date '1998-03-01'
 
-where c_birth_country is not null 
+    where c_birth_country is not null 
 
-group by 1 
-order by 1
+    group by 1 
+    order by 1
