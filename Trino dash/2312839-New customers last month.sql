09cbdea793fd98132885e717c3be79db27891c80
Jake Peterson <jake@popsql.com>
2023-10-03 15:08:24 +0000

    Update query `New customers last month`

--- Trino dash/2312839-New customers last month.sql
+++ Trino dash/2312839-New customers last month.sql
@@ -33,6 +33,6 @@ total as (
 select 
     total.d_date, 
     new_customers, 
-    total_customers-new_customers as returning_customers 
+    total_customers - new_customers as returning_customers 
 from total, new 
 where total.d_date = new.d_date
