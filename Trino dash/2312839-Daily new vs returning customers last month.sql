4038b7deca3bf6dba99a89ce649c19a5e6f23df7
Jake Peterson <jake@popsql.com>
2023-10-05 15:45:27 +0000

    Move query `Daily new vs returning customers last month` (part 2/2)

---
+++ Trino dash/2312839-Daily new vs returning customers last month.sql
@@ -0,0 +1,39 @@
+with new as (
+    select 
+        dd.d_date,
+        count(1) as new_customers 
+
+    from sf1.customer c 
+        
+        inner join sf1.date_dim dd 
+            on dd.d_date_sk = c.c_first_sales_date_sk 
+            and dd.d_date >= date '1998-02-01'
+            and dd.d_date < date '1998-03-01'
+
+    where c_birth_country is not null 
+
+    group by 1 
+), 
+
+total as (
+
+    select 
+        dd.d_date,
+        count(distinct ss.ss_customer_sk) as total_customers
+ 
+    from sf1.store_sales ss 
+
+        inner join sf1.date_dim dd on dd.d_date_sk = ss.ss_sold_date_sk 
+            and dd.d_date >= date '{{from_date}}'
+            and dd.d_date <= date '{{to_date}}'
+
+    group by 1 
+)
+
+select 
+    total.d_date, 
+    new_customers, 
+    total_customers - new_customers as returning_customers 
+
+from total, new 
+where total.d_date = new.d_date
