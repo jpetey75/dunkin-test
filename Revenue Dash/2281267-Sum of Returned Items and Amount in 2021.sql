80d27644eb40f8386cda1ddaa7de70bc0fcaef08
Jake Peterson <jake@popsql.com>
2023-12-12 19:18:51 +0000

    Update query `Sum of Returned Items and Amount in 2021`

--- Revenue Dash/2281267-Sum of Returned Items and Amount in 2021.sql
+++ Revenue Dash/2281267-Sum of Returned Items and Amount in 2021.sql
@@ -9,7 +9,7 @@ with returned_items as (
         oi.returned_at,
         cast(round(oi.sale_price, 2) as numeric) as returned_amount
 
-    from ecommerce.order_items oi 
+    from ecommerce.order_items as oi 
     where
         oi.returned_at is not null
         and oi.status = 'Returned'
